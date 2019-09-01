80ff147c84501bf5a2ad5a71efc37c6f853b7d34
Thomas Lock <thomas.lock@probablyrational.com>
2019-09-01 13:31:39 +0100

    initial version

---
+++ api.php
@@ -0,0 +1,75 @@
+<?php
+    require "connect.php";
+    require "functions.php";
+    $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
+
+    $apiKey = "";
+    $output = false;
+
+    foreach (getallheaders() as $name => $value) {
+        if ($name == "Authorisation") {
+            $apiKey = $value;
+        }
+    }
+
+    switch ($request_uri[0]) {
+        case 'get':
+            getScoreboard();
+            break;
+        case 'update':
+            updatedScoreboard();
+            break;
+        default:
+            header('HTTP/1.0 404 Not Found');
+            echo 'Request not found.';
+            break;
+    }
+
+    function getScoreboard() {
+        try {
+            $scoreboard = $_POST['scoreboard'];
+            $limit = intval($_POST['limit']);
+            $upper = intval($_POST['upper']);
+
+            $sql = "SELECT * FROM score WHERE reference='".$scoreboard."' ORDER BY score ASC";
+            $statement = $pdo->prepare($sql);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            $displayResults;
+
+            for ($i = $limit; $i < $limit + $upper; $i++) {
+                $displayResults += $results[$i];
+            }
+
+            echo '{ok: true, data: '.json_encode($displayResults).'}';
+        } catch(Exception $e) {
+            echo '{ok: false, message: '.$e->getMessage().'}';
+        }
+    }
+
+    function updatedScoreboard() {
+        try {
+            $sql = "INSERT INTO score (reference,player,score,uniqueId) VALUES ('".$_POST['scoreboard']."', '".$_POST['player']."', ".$_POST['score'].", '".$_POST['uniqueId']."')";
+            $statement = $pdo->prepare($sql);
+            $statement->execute();
+
+            $sql = "SELECT * FROM score WHERE reference='".$scoreboard."' ORDER BY score ASC";
+            $statement = $pdo->prepare($sql);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            $position = 1;
+
+            for ($i = 0; $i < count(array_len); $i++) {
+                if ($results[$i]["uniqueId"] == $_POST['uniqueId']) {
+                    $position = i;
+                }
+            }
+
+            echo "{ok: true, position: ".$position."}";
+        } catch(Exception $e) {
+            echo '{ok: false, message: '.$e->getMessage().'}';
+        }
+    }
+?>
---
+++ setup.php
@@ -0,0 +1,41 @@
+<?php
+    if ($_POST['dbname']) {
+        $authFile = fopen("auth.php", "w");
+        $authWrite = '
+        <?php
+            $mysql_username = "'.$_POST["username"].'";
+            $mysql_password = "'.$_POST["password"].'";
+            $mysql_server = "'.$_POST["server"].'";
+            $mysql_dbname = "'.$_POST["dbname"].'";
+        ?>';
+        fwrite( $authFile, $authWrite );
+        require "connect.php";
+        echo "<h1>Setup</h1>";
+        echo "<h2>Database</h2>";
+
+        echo "<h3>Score</h3>";
+        $sql = "CREATE TABLE score (
+            id int NOT NULL AUTO_INCREMENT,
+            reference text NOT NULL,
+            player text NOT NULL,
+            score text,
+            uniqueId text,
+            PRIMARY KEY (id)
+        );";
+        $query = $pdo->prepare($sql);
+        $query->execute();  
+
+        echo "<h2>Setup complete. Please delete setup.php from your server.</h2>";
+    } else {
+?>
+    <h1>Setup</h1>
+    <h2>Please enter database details</h2>
+    
+    <form method="POST" action="setup.php">
+        Username: <input type="text" name="username" default="mysql_username" /> <br />
+        Password: <input type="password" name="password" default="password" /> <br />
+        Server address: <input type="text" name="server" default="mysql_server" /> <br />
+        Database name: <input type="text" name="dbname" default="mysql_dbname" /> <br />
+        <input type="submit" value="Install" /> <br />
+    </form>
+    <? } ?>
